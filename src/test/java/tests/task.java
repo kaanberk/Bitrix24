778071efe360e9c5d48d3a354278cdef5794e206
Kaan <kaan.kocak41@gmail.com>
2019-12-09 18:16:14 +0000

    add void method

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,11 +1,14 @@
 package tests;
 
 public class task {
-  
-  public static void main(String[] args){
-    
-   System.out.println("Hello World");
-     
 
-  }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+    public void h1(){
+        System.out.println("Hi from Intellij");
+    }
+
 }
